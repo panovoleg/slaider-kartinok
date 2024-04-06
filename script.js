247f23dcbcd77f18dc073f6b164912b517587f5f
panovoleg <149285809+panovoleg@users.noreply.github.com>
2024-04-06 22:12:49 +0500

    загрузка проекта

---
+++ script.js
@@ -0,0 +1,14 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+let slider =document.querySelector('#slider')
+const url1="http://wallpapers-image.ru/2560x1600/cars/wallpapers-cars-019.jpg";
+const url2="http://wallpapers-image.ru/2560x1600/cars/wallpapers-cars-023.jpg";
+const url3="http://wallpapers-image.ru/2560x1600/cars/wallpapers-cars-09.jpg";
+let image =['url1','url2','url3'];
+left.addEventListener;('click',()=>{
+    event.preventDefault();
+})
+right.addEventListener;('click',()=>{
+    event.preventDefault();
+})
